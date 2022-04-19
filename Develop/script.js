f3fed1fee8756e2d2dfe4b50f73f3e2a791de661
your name <mrmansaray12@gmail.com>
2022-04-19 13:06:45 -0400

    update

---
+++ Develop/script.js
@@ -0,0 +1,37 @@
+$(".saveBtn").on("click", function () {
+  var textbox = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+  console.log(textbox, time);
+  localStorage.setItem(time, textbox);
+});
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-1 .description").val(localStorage.getItem("hour-1"));
+$("#hour-2 .description").val(localStorage.getItem("hour-2"));
+$("#hour-3 .description").val(localStorage.getItem("hour-3"));
+$("#hour-4 .description").val(localStorage.getItem("hour-4"));
+$("#hour-5 .description").val(localStorage.getItem("hour-5"));
+
+function textboxcolors() {
+  var currentHour = moment().hours();
+
+  var date = moment().format("MMMM Do YYYY");
+  document.getElementById("currentDay").innerHTML = date;
+  $(".time-block").each(function () {
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
+}
+setTimeout(textboxcolors, 1000);
